b5594d52afbacba6edf530fb08d5cf56fc75c2f1
vitalii-samoilenko <vitalii_samoilenko@hotmail.com>
2026-02-03 00:57:43 +0200

    Palette builder

---
+++ src/Voicemeeter.Clients.UI.Palette.Oklab/include/Voicemeeter/Clients/UI/Palette.hpp
@@ -0,0 +1,33 @@
+#ifndef VOICEMEETER_CLIENTS_UI_PALETTE_HPP
+#define VOICEMEETER_CLIENTS_UI_PALETTE_HPP
+
+#include <memory>
+
+#include "Voicemeeter/UI/Graphics/Palette.hpp"
+
+namespace Voicemeeter {
+	namespace Clients {
+		namespace UI {
+			using Palette = UI::Graphics::Palette;
+
+			class PaletteBuilder {
+			public:
+				inline PaletteBuilder() = default;
+				PaletteBuilder(PaletteBuilder const &) = delete;
+				PaletteBuilder(PaletteBuilder &&) = delete;
+
+				inline ~PaletteBuilder() = default;
+
+				PaletteBuilder & operator=(PaletteBuilder const &) = delete;
+				PaletteBuilder & operator=(PaletteBuilder &&) = delete;
+
+			protected:
+				inline ::std::unique_ptr<Palette> Build() {
+					return ::std::make_unique<Palette>();
+				};
+			}
+		}
+	}
+}
+
+#endif
