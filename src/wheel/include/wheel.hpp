99d66422ba6ff7de0c245f8be43e6c986c7f2e02
vitalii-samoilenko <vitalii_samoilenko@hotmail.com>
2026-02-16 08:23:39 +0200

    Wheel refine

--- src/wheel/include/wheel.hpp
+++ src/wheel/include/wheel.hpp
@@ -2,53 +2,61 @@
 #define WHEEL_HPP
 
 #include <cmath>
-#include <valarray>
+#include <cstdint>
+#include <limits>
 #include <type_traits>
+#include <valarray>
 
+#ifdef _WHEEL_EXTENDED_PRECISION
 using num_t = int64_t;
-
-#define _PRECISION 8
-#define _INT_MASK 0b1111111111111111111111111111111111111111111111111111111100000000
-/*
-#define _INT_MASK 0b11111111111111111111111100000000
-*/
-#define _MAX_SQRT 46340
+using unum_t = uint64_t;
+constexpr int _wheel_Precision{ 8 };
+constexpr num_t _wheel_Msqrt{ 3037000499 };
+#else
+using num_t = int32_t;
+using unum_t = uint32_t;
+constexpr int _wheel_Precision{ 6 };
+constexpr num_t _wheel_Msqrt{ 46340 };
+#endif
 
 template<typename T,
 	::std::enable_if_t<
 		::std::is_integral_v<T>,
 		bool> = true>
 inline constexpr num_t push(T n) {
-	return static_cast<num_t>(n) << _PRECISION;
+	return static_cast<num_t>(n)
+		<< _wheel_Precision;
 };
 template<typename T,
 	::std::enable_if_t<
 		::std::is_integral_v<T>,
 		bool> = true>
 inline constexpr num_t pop(T n) {
-	return static_cast<num_t>(n) >> _PRECISION;
+	return static_cast<num_t>(n)
+		>> _wheel_Precision;
 };
 
 inline constexpr bool is_safe(num_t n) {
-	return -(_MAX_SQRT + 1) < n
-		&& n < _MAX_SQRT + 1;
+	return -(_wheel_Msqrt + 1) < n
+		&& n < _wheel_Msqrt + 1;
 };
 
 constexpr num_t Zero{ 0 };
 constexpr num_t One{ push(1) };
-constexpr num_t Inf{ _MAX_SQRT * _MAX_SQRT + 1 };
+constexpr num_t Inf{ _wheel_Msqrt * _wheel_Msqrt + 1 };
 
-inline constexpr num_t trunc(num_t n) {
-	return n & _INT_MASK;
-};
-inline constexpr num_t frac(num_t n) {
-	return n - trunc(n);
-};
-inline constexpr num_t ceil(num_t n) {
-	return trunc(n + One - 1);
+constexpr unum_t _wheel_Imask{
+	::std::numeric_limits<unum_t>::max()
+		<< _wheel_Precision
 };
 inline constexpr num_t floor(num_t n) {
-	return -ceil(-n);
+	return n & _wheel_Imask;
+};
+inline constexpr num_t ceil(num_t n) {
+	return floor(n + One - 1);
+};
+inline constexpr num_t frac(num_t n) {
+	return n & ~_wheel_Imask;
 };
 
 inline num_t ans(num_t n) {
@@ -77,14 +85,14 @@ template<typename V,
 		!::std::is_integral_v<V>,
 		bool> = true>
 inline auto push(V const &v) {
-	return v << _PRECISION;
+	return v << _wheel_Precision;
 };
 template<typename V,
 	::std::enable_if_t<
 		!::std::is_integral_v<V>,
 		bool> = true>
 inline auto pop(V const &v) {
-	return v >> _PRECISION;
+	return v >> _wheel_Precision;
 };
 template<typename V,
 	::std::enable_if_t<
@@ -121,5 +129,6 @@ template<typename V,
 inline num_t all(V const &v) {
 	return min(v);
 };
+}
 
 #endif
